6b83806ec15d182d6ea8bda114cc1a04bd5187b8
Maria Simoes <msmores85@gmail.com>
2019-10-09 21:40:32 -0400

    Addition of div elements with background colors

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 
 
-document.body.style.backgroundColor = "#D3D3D3";
+document.body.style.backgroundColor = "#E8E8E8";
 document.body.children[0].style.textAlign = "center";
 
+document.getElementById("container").style.backgroundColor = "#F8F8FF";
+document.getElementById("container").style.height = "400px";
+document.getElementById("container").style.width = "700px";
+document.getElementById("container").style.padding = "10px";
+document.getElementById("container").style.margin = "50px";
+//Code to create rounded edges for the border from: https://www.w3schools.com/css/css3_borders.asp
+document.getElementById("container").style.borderRadius = "15px";
+
+
+document.getElementById("container2").style.backgroundColor ="white";
+//Code to create rounded edges for the border from: https://www.w3schools.com/css/css3_borders.asp
+document.getElementById("container2").style.borderRadius = "15px";
+
+
+
+document.getElementById("password").style.objectFit = "cover";
 
 
 
